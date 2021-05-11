52ca6787978bd8404ca71897dffc14680ea223a9
Mikhael6363 <mikhael.6363@gmail.com>
2021-05-11 18:28:50 +0300

    Add path configs and db.json

---
+++ config/config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const { env } = require('process');
+
+module.exports = {
+  STATIC_PATH: env.STATIC_PATH || path.resolve(__dirname, '../public'),
+  DB_CONFIG: path.resolve(__dirname, 'db.json'),
+  PORT: process.env.PORT || 3000,
+};
