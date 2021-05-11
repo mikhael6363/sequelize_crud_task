8a58517a7b5d0b7f7ddf4f45da352cee52d01485
Mikhael6363 <mikhael.6363@gmail.com>
2021-05-11 18:27:41 +0300

    Add app & server changes

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = require('./router');
+const errorHandler = require('./middlewares/error.handlers');
+const { STATIC_PATH } = require('./config/config');
 
 const app = express();
 
-app.use(express.json()); // data stream -> json -> js object -> req.body
-/* 
-  http://localhost:3000/api/*
- */
+app.use(express.static(STATIC_PATH));
+app.use(express.json());
 app.use('/api', router);
-
-app.use((err, req, res, next) => {
-  res.status(500).send({
-    errors: [{ message: err.message }],
-  });
-});
+app.use(errorHandler);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,24 +1,9 @@
 const http = require('http');
 const app = require('./app.js');
+const { PORT } = require('./config');
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log(`APP started on port ${port}`);
+server.listen(PORT, () => {
+  console.log(`APP started on port ${PORT}`);
 });
-
-
-
-
-/* 
-
-
-            СОЗДАТЬ 5 НОВЫХ МОДЕЛЕЙ И МИГРАЦИЙ. МИНИМУМ 5 АТРИБУТОВ.
-            
-            npx sequelize --help
-
-*/
-
-
