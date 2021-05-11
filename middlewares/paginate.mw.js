7a4e0caaa4687ae85729de96b0a76f19ee02355d
Mikhael6363 <mikhael.6363@gmail.com>
2021-05-11 18:31:56 +0300

    Add pagination

---
+++ middlewares/paginate.mw.js
@@ -0,0 +1,16 @@
+module.exports = async (req, res, next) => {
+  try {
+    const {
+      query: { limit, offset },
+    } = req;
+
+    req.pagination = {
+      limit: !limit || limit < 5 || limit <= 0 ? 5 : limit,
+      offset: !offset || offset <= 0 ? 0 : offset,
+    };
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
